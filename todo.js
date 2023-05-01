925d959fb5c15fe9630a3129e51b2ebeaf2eafb9
Ashwani Kumar Dewangan <ashwanidewangan31@gmail.com>
2023-05-01 11:05:49 +0530

    added listners to the code

--- todo.js
+++ todo.js
@@ -4,12 +4,12 @@ const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
 console.log('Working');
-function addTaskToDom(task){
+function addTaskToDom(task) {
     const li = document.createElement('li');
 
     li.innerHTML = `
   
-          <input type="checkbox" id=${task.id} data-id="12" ${task.done? 'checked' : ''}  class="custom-checkbox">
+          <input type="checkbox" id=${task.id} data-id="12" ${task.done ? 'checked' : ''}  class="custom-checkbox">
           <label for=${task.id}>${task.text}</label>
           <img src="delete.png" class="delete" alt='delete' data-id=${task.id} />
    
@@ -19,20 +19,20 @@ function addTaskToDom(task){
 }
 
 
-function renderList () {
+function renderList() {
     taskList.innerHTML = '';
-    for(let i=0; i<tasks.length; i++){
+    for (let i = 0; i < tasks.length; i++) {
         addTaskToDom(tasks[i])
     }
 }
 
 
 // isme thoda doubt hai
-function toggleTak (taskId) {
-    const task = task.filter(function (task){
+function toggleTak(taskId) {
+    const task = task.filter(function (task) {
         return task.id === taskId;
     })
-    if(task.length > 0){
+    if (task.length > 0) {
         const currentTaks = task[0];
 
         currentTaks.done = !currentTaks.done;
@@ -42,8 +42,8 @@ function toggleTak (taskId) {
     }
 }
 
-function deleteTask (taskId) {
-    const newTasks = tasks.filter(function (task){
+function deleteTask(taskId) {
+    const newTasks = tasks.filter(function (task) {
         return task.id !== taskId;
     })
     tasks = newTasks;
@@ -51,8 +51,8 @@ function deleteTask (taskId) {
     showNotification('task deleted succesfully');
 }
 
-function addTask (task) {
-    if(task){
+function addTask(task) {
+    if (task) {
         tasks.push(task);
         renderList()
         showNotification('task added succesfully')
@@ -65,18 +65,18 @@ function showNotification(text) {
     alert(text)
 }
 
-function handleInputKeypress(e){
-    if(e.key == 'Enter'){
+function handleInputKeypress(e) {
+    if (e.key == 'Enter') {
         const text = e.target.value;
         console.log(text)
-        if(!text){
+        if (!text) {
             showNotification('Task cannot be empty')
             return;
         }
         const task = {
-            text : text,
-            id : Date.now().toString(),
-            done : false
+            text: text,
+            id: Date.now().toString(),
+            done: false
 
         }
         e.target.value = ``;
@@ -84,4 +84,25 @@ function handleInputKeypress(e){
     }
 
 }
-addTaskInput.addEventListener('keyup', handleInputKeypress);
+// handleClickListner
+function handleClickListner(e){
+  const target = e.target;
+  if(target.className === 'delete'){
+        const taskId = target.dataset.id;
+        deleteTask()
+        return
+  }else if(target.className === 'custom-checkbox'){
+    const taskId = target.id;
+    toggleTak()
+    return
+  }
+}
+
+function listeners(){
+    addTaskInput.addEventListener('keyup', handleInputKeypress);
+    document.addEventListener('click', handleClickListner)
+}
+
+listeners()
+
+
